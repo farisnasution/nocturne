6638346bdfb1fc57c3e98c6d970c41e417324908
Faris Nasution <nasution.faris@gmail.com>
2015-04-17 16:13:43 +0700

    Add several utility function to ease the pain getting specific [data]/[chart]/[showcase]

--- src/cljs/nocturne/util/state.cljs
+++ src/cljs/nocturne/util/state.cljs
@@ -20,3 +20,31 @@
 (defn get-content
   []
   (:content @state/app-state))
+
+(defn get-user
+  [user-slug]
+  (get-in @state/app-state [:users user-slug]))
+
+(defn get-datas
+  [user-slug]
+  (get-in @state/app-state [:users user-slug :data]))
+
+(defn get-charts
+  [user-slug]
+  (get-in @state/app-state [:users user-slug :chart]))
+
+(defn get-showcases
+  [user-slug]
+  (get-in @state/app-state [:users user-slug :showcase]))
+
+(defn get-data
+  [user-slug data-slug]
+  (get-in @state/app-state [:users user-slug :data data-slug]))
+
+(defn get-chart
+  [user-slug chart-slug]
+  (get-in @state/app-state [:users user-slug :chart chart-slug]))
+
+(defn get-showcase
+  [user-slug showcase-slug]
+  (get-in @state/app-state [:users user-slug :showcase showcase-slug]))
