8fa1828c8d0f671bb27fe76a06c1bffec498eced
Faris Nasution <nasution.faris@gmail.com>
2015-04-17 15:13:09 +0700

    Every ajax request now returns a channel containing the result.

--- src/cljs/nocturne/util/io.cljs
+++ src/cljs/nocturne/util/io.cljs
@@ -1,3 +1,43 @@
 (ns cljs.nocturne.util.io
   (:require [ajax.core :as ajax])
   (:use [cljs.core.async :only [put! chan]]))
+
+(defn ajax-config
+  [ch params headers]
+  (cond-> {:handler #(put! ch [:ok %])
+           :error-handler #(put! ch [:error %])
+           :response-format :transit}
+          (map? params) (assoc :params params)
+          (map? headers) (assoc :headers headers)))
+
+(defn get-request
+  [url params headers]
+  (let [ch (chan)
+        config (ajax-config ch params headers)]
+    (do
+      (ajax/GET url config)
+      ch)))
+
+(defn post-request
+  [url params headers]
+  (let [ch (chan)
+        config (ajax-config ch params headers)]
+    (do
+      (ajax/POST url config)
+      ch)))
+
+(defn put-request
+  [url params headers]
+  (let [ch (chan)
+        config (ajax-config ch params headers)]
+    (do
+      (ajax/PUT url config)
+      ch)))
+
+(defn delete-request
+  [url params headers]
+  (let [ch (chan)
+        config (ajax-config ch params headers)]
+    (do
+      (ajax/DELETE url config)
+      ch)))
