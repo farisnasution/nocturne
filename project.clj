4749cc2ea8f9f6cce808b1a17c048c55418a9974
Faris Nasution <nasution.faris@gmail.com>
2015-04-26 09:23:11 +0700

    Bump version, fix aliases on [au] and change port of [figwheel] to 3449.

--- project.clj
+++ project.clj
@@ -4,31 +4,31 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3165"]
+                 [org.clojure/clojurescript "0.0-3211"]
                  [org.omcljs/om "0.8.8"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [figwheel "0.2.5"]
+                 [figwheel "0.2.6"]
                  [hiccup "1.0.5"]
                  [sablono "0.3.4"]
-                 [secretary "1.2.2"]
-                 [cljs-ajax "0.3.10"]
+                 [secretary "1.2.3"]
+                 [cljs-ajax "0.3.11"]
                  [org.clojars.fterrier/om-autocomplete "0.1.0"]]
   :profiles {:dev {:plugins [[lein-cljsbuild "1.0.5"]
-                             [lein-figwheel "0.2.5"]
+                             [lein-figwheel "0.2.6"]
                              [hiccup-watch "0.1.2"]
                              [lein-haml-sass "0.2.7-SNAPSHOT"]
                              [jonase/eastwood "0.2.1"]
-                             [lein-ancient "0.6.5"]
-                             [lein-kibit "0.0.8"]
+                             [lein-ancient "0.6.7"]
+                             [lein-kibit "0.1.2"]
                              [lein-bikeshed "0.2.0"]]}}
   :figwheel {:http-server-root "public"
-             :port 8000
+             :port 3449
              :css-dirs ["resources/public/css"]}
   :jvm-opts ["-Xmx1G"]
   :aliases {"dev" ["figwheel" "dev"]
             "hw" ["hiccup-watch"]
-            "omni" ["do" ["ancient"] ["kibit"] ["eastwood"] ["bikeshed"]]
-            "au" ["ancient" ":all" "upgrade" ":interactive"]}
+            "omni" ["do" ["au"] ["kibit"] ["eastwood"] ["bikeshed"]]
+            "au" ["ancient" "upgrade" ":all"]}
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src/cljs"
                                        "src/figwheel"]
