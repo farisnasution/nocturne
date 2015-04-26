4a2d6dd97e0bc2f8696da1d720d08353401f84bd
Faris Nasution <nasution.faris@gmail.com>
2015-04-26 09:27:38 +0700

    Add more routes.

--- src/cljs/nocturne/app/user/route.cljs
+++ src/cljs/nocturne/app/user/route.cljs
@@ -9,3 +9,17 @@
          (fn [current us]
            (assoc current :content [:overview :read [us]]))
          user-slug))
+
+(defroute user-settings
+  "/settings"
+  []
+  (swap! app-state
+         (fn [current]
+           (assoc current :content [:settings]))))
+
+(defroute user-logout
+  "/logout"
+  []
+  (swap! app-state
+         (fn [current]
+           (assoc current :content [:logout]))))
