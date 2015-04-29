5fb449ba661c56082307f64ea1d42016091a8bf8
Faris Nasution <nasution.faris@gmail.com>
2015-04-29 14:47:53 +0700

    Add interface to show and edit [data]

--- src/cljs/nocturne/app/data/component/show_data.cljs
+++ src/cljs/nocturne/app/data/component/show_data.cljs
@@ -5,7 +5,7 @@
             [cljs.core.async :as async :refer [<! put! chan]])
   (:use [cljs.nocturne.app.component.field.input :only [input-field]]
         [cljs.nocturne.app.component.field.textarea :only [textarea-field]]
-        [cljs.nocturne.app.component.field.validated :only [validated-field]])
+        [cljs.nocturne.app.component.editable-dd :only [editable-dd]])
   (:use-macros [cljs.nocturne.macro :only [defcomponent]]
                [cljs.core.async.macros :only [go-loop go]]))
 
@@ -15,80 +15,33 @@
   (render [_]
           [:dt {}
            content]))
-r
-(defcomponent dd
-  [{:keys [content]} owner]
-  (display-name [_] "dd")
-  (render [_]
-          [:dd {}
-           content]))
-
-(defn handle-attempt-to-edit
-  [data id owner error value event]
-  (condp
-      (and (nil? error)
-           (= (:ENTER ue/keycodes)
-              (ue/keycode event))) (do
-                                     (om/update! data id value)
-                                     (om/set-state! owner :editing? false))
-    (= (:ESC ue/keycodes) (ue/keycode event)) (om/set-state! owner
-                                                             :editing?
-                                                             false)))
-
-(defcomponent editable-dd
-  "TODO: val-fns"
-  [data owner {:keys [id
-                      error-title]}]
-  (display-name [_] "editable-dd")
-  (init-state [_]
-              (let [ch (chan)]
-                {:ch ch
-                 :callback-fn #(put! ch [:self nil %])
-                 :editing? false}))
-  (will-mount [_]
-              (let [ch (om/get-state owner :ch)]
-                (go-loop []
-                  (let [[from meta-info value] (<! ch)]
-                    (condp = from
-                      :self (om/set-state! owner :editing? true)
-                      :name (let [error meta-info
-                                  [real-value event] value]
-                              (handle-attempt-to-edit data
-                                                      id
-                                                      owner
-                                                      error
-                                                      real-value
-                                                      event))))
-                  (recur))))
-  (did-mount [_]
-             (let [callback-fn (om/get-state owner :callback-fn)
-                   node (om/get-node owner)]
-               (ue/listen node (:DBLCLICK ue/event-type) callback-fn)))
-  (will-unmount [_]
-                (let [callback-fn (om/get-state owner :callback-fn)
-                      node (om/get-node owner)]
-                  (ue/unlisten node (:DBLCLICK ue/event-type) callback-fn)))
-  (render-state [_ {:keys [editing? ch]}]
-                (if editing?
-                  (om/build validated-field
-                            {:content (id data)}
-                            {:opts {:parent-ch ch
-                                    :val-fns [[#(not (empty? %))
-                                               (str error-title " cannot empty.")]]
-                                    :id id
-                                    :view  input-field
-                                    :view-opts {:field-type "text"}}})
-                  (om/build dd {:content (id data)}))))
 
 (defcomponent data-dl
   [data owner]
   (display-name [_] "dl")
   (render [_]
-          [:dl {}
+          [:dl {:class "dl-horizontal"}
            (om/build dt {:content "Name"})
-           (om/build editable-dd data {:opts {:id :name}})
+           (om/build editable-dd
+                     data
+                     {:opts {:ks [:name]
+                             :val-fns [[#(not (empty? %))
+                                        "fjewhfewjfhewfhjj"]]
+                             :id :name
+                             :view input-field
+                             :view-opts {:field-type "text"}}})
            (om/build dt {:content "Url"})
-           (om/build editable-dd data {:opts {:id :url}})
+           (om/build editable-dd
+                     data
+                     {:opts {:ks [:url]
+                             :val-fns []
+                             :id :url
+                             :view input-field
+                             :view-opts {:field-type "text"}}})
            (om/build dt {:content "Description"})
-           (om/build dd {:content description})
-           (om/build editable-dd data {:opts {:id :description}})]))
+           (om/build editable-dd
+                     data
+                     {:opts {:ks [:description]
+                             :val-fns []
+                             :id :description
+                             :view textarea-field}})]))
