cee3c2ac97ccb85b835567872dfb89e479921aca
Faris Nasution <nasution.faris@gmail.com>
2015-05-06 10:35:58 +0700

    [show-data] stop being editable.

--- src/cljs/nocturne/app/data/component/show_data.cljs
+++ src/cljs/nocturne/app/data/component/show_data.cljs
@@ -16,32 +16,63 @@
           [:dt {}
            content]))
 
+(defcomponent dd
+  [{:keys [content]} owner]
+  (display-name [_] "dd")
+  (render [_]
+          [:dd {}
+           content]))
+
 (defcomponent data-dl
   [data owner]
   (display-name [_] "dl")
   (render [_]
           [:dl {:class "dl-horizontal"}
-           (om/build dt {:content "Name"})
-           (om/build editable-dd
-                     data
-                     {:opts {:ks [:name]
-                             :val-fns [[#(not (empty? %))
-                                        "fjewhfewjfhewfhjj"]]
-                             :id :name
-                             :view input-field
-                             :view-opts {:field-type "text"}}})
-           (om/build dt {:content "Url"})
-           (om/build editable-dd
-                     data
-                     {:opts {:ks [:url]
-                             :val-fns []
-                             :id :url
-                             :view input-field
-                             :view-opts {:field-type "text"}}})
-           (om/build dt {:content "Description"})
-           (om/build editable-dd
-                     data
-                     {:opts {:ks [:description]
-                             :val-fns []
-                             :id :description
-                             :view textarea-field}})]))
+           (om/build dt
+                     {:content "Name"}
+                     {:react-key "name-dt"})
+           (om/build dd
+                     {:content (:name data)}
+                     {:react-key "name-dd"})
+           (om/build dt
+                     {:content "Url"}
+                     {:react-key "url-dt"})
+           (om/build dd
+                     {:content (:url data)}
+                     {:react-key "url-dd"})
+           (om/build dt
+                     {:content "Description"}
+                     {:react-key "description-dt"})
+           (om/build dd
+                     {:content (:description data)}
+                     {:react-key "description-dd"})]))
+
+;; (defcomponent data-dl
+;;   [data owner]
+;;   (display-name [_] "dl")
+;;   (render [_]
+;;           [:dl {:class "dl-horizontal"}
+;;            (om/build dt {:content "Name"})
+;;            (om/build editable-dd
+;;                      data
+;;                      {:opts {:ks [:name]
+;;                              :val-fns [[#(not (empty? %))
+;;                                         "fjewhfewjfhewfhjj"]]
+;;                              :id :name
+;;                              :view input-field
+;;                              :view-opts {:field-type "text"}}})
+;;            (om/build dt {:content "Url"})
+;;            (om/build editable-dd
+;;                      data
+;;                      {:opts {:ks [:url]
+;;                              :val-fns []
+;;                              :id :url
+;;                              :view input-field
+;;                              :view-opts {:field-type "text"}}})
+;;            (om/build dt {:content "Description"})
+;;            (om/build editable-dd
+;;                      data
+;;                      {:opts {:ks [:description]
+;;                              :val-fns []
+;;                              :id :description
+;;                              :view textarea-field}})]))
