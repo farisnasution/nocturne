ae4fbe305e271fc678f5563c47cf133393450b08
Faris Nasution <nasution.faris@gmail.com>
2015-05-06 17:52:32 +0700

    Break down [get-request] fn, each for internal and external api call.

--- src/cljs/nocturne/util/io.cljs
+++ src/cljs/nocturne/util/io.cljs
@@ -2,23 +2,39 @@
   (:require [ajax.core :as ajax])
   (:use [cljs.core.async :only [put! chan]]))
 
+(defn external-ajax-config
+  [ch params headers]
+  (cond-> {:handler #(put! ch [:ok %])
+           :error-handler #(put! ch [:fail %])
+           :response-format :json
+           :format :json
+           :keywords? true}
+          (map? params) (assoc :params params)
+          (map? headers) (assoc :headers headers)))
+
 (defn ajax-config
   [ch params headers]
   (cond-> {:handler #(put! ch [:ok %])
            :error-handler #(put! ch [:fail %])
-           :response-format :transit}
+           :response-format :transit
+           :format :transit}
           (map? params) (assoc :params params)
           (map? headers) (assoc :headers headers)))
 
-(defn get-request
-  [url {:keys [params headers]} & [channel]]
-  (let [ch (if (nil? channel)
-             (chan)
-             channel)
-        config (ajax-config ch params headers)]
-    (do
-      (ajax/GET url config)
-      ch)))
+(defn base-get-request
+  [config-fn]
+  (fn [url {:keys [params headers]} & [channel]]
+    (let [ch (if (nil? channel)
+               (chan)
+               channel)
+          config (config-fn ch params headers)]
+      (do
+        (ajax/GET url config)
+        ch))))
+
+(def get-request (base-get-request ajax-config))
+
+(def external-get-request (base-get-request external-ajax-config))
 
 (defn post-request
   [url {:keys [params headers]} & [channel]]
